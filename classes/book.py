05ceb01dbf71e55efaa41b1c24a0fc17cd27ef9d
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-10 17:51:08 +0200

    finish the class library Haim part

--- classes/book.py
+++ classes/book.py
@@ -1,10 +1,10 @@
 class Book:
 
-    def __init__(self,title,author,isbn,is_availabe=True):
+    def __init__(self,title,author,isbn):
         self.title = title
         self.author = author
         self.isbn = isbn 
-        self.is_available = is_availabe
+        self.is_available = True
 
     def __str__(self):
         return f"Book: {self.title} by {self.author} number:{self.isbn}, is_availble:{self.is_available}"
--- classes/library.py
+++ classes/library.py
@@ -1,13 +1,46 @@
+from classes.book import Book
+from classes.user import User
+
+
 class Library:
     def __init__(self, books: list, users: list):
         self.books = books
         self.users = users
 
     def add_book(self, book):
-        self.books.append(book)
+        self.books.append(book.isbn)
 
     def add_user(self, user):
-        self.users.append(user)
+        self.users.append(user.name)
 
-    def borrow_book(self, user_id):
-        self.users.append(user_id)
+    @staticmethod
+    def borrow_book(user, book_isbn, book):
+        if book.is_available:
+            user.append(book_isbn)
+            book.is_available = False
+        else:
+            print("the book is already borrowed")
+
+    @staticmethod
+    def return_book(user, book_isbn, book):
+        if book_isbn in user.borrowed_books:
+            book.is_available = True
+            user.borrowed_books.remove(book_isbn)
+        else:
+            print("you didn't have this book")
+
+
+    def set_available_books(self):
+        available_book = []
+        for i in self.books:
+            if i.is_available:
+                available_book.append(i.isbn)
+        return available_book
+
+    def search_book(self,author):
+        book_by_author = []
+        for i in self.books:
+            if i == author:
+                book_by_author.append(i)
+        return book_by_author
+
