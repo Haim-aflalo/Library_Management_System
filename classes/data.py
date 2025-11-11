4636ac04814d1378d97beb5d306c6ab9c7d72041
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-11 12:15:40 +0200

    excuse me error

--- classes/data.py
+++ classes/data.py
@@ -1,10 +1,6 @@
 import json
+import pandas as pd
 
-
-def read_from_json():
-    with open ("data.json" , "r") as f:
-        t = f.read()
-        json.loads()
 
 
 class SaveData:
@@ -30,30 +26,36 @@ class SaveData:
         return books_lst
 
     def save_to_json(self):
-       with open ("data.json" , "a") as f:
+       with open ("../data/data.json", "a") as f:
         books = self.convert_books_lst(self.books)
         users = self.convert_users_lst(self.users)
         json.dump(books,f)
         json.dump(users,f)
 
+    # @staticmethod
+    # def json_to_csv():
+    #     with open("../data/data.json", encoding='utf-8') as inputfile:
+    #         df = pd.read_json(inputfile)
+    #
+    #     df.to_csv("data.csv", encoding='utf-8', index=False)
 
-from book import Book
-from library import Library
-from user import User
 
-
-b1 = Book("hello","david",1234)
-b2 = Book("hello_p","david_p",12345)
-u1= User("leo",456)
-u2= User("leo_p",4567)
-l = Library()
-l.add_book(b1)
-l.add_book(b2)
-l.add_user(u1)
-l.add_user(u2)
-l.borrow_book(u1,b1)
-
-s = SaveData(l.books,l.users)
-print(s.convert_books_lst(l.books))
-s.save_to_json()
-read_from_json()
+#
+# from book import Book
+# from library import Library
+# from user import User
+#
+#
+# b1 = Book("hello","david",1234)
+# b2 = Book("hello_p","david_p",12345)
+# u1= User("leo",456)
+# u2= User("leo_p",4567)
+# l = Library()
+# l.add_book(b1)
+# l.add_book(b2)
+# l.add_user(u1)
+# l.add_user(u2)
+# l.borrow_book(u1,b1)
+# s = SaveData(l.books,l.users)
+# s.save_to_json()
+# # s.json_to_csv()
