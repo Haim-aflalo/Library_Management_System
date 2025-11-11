803712ed2fab2d8849192607d531f31d0fffda0d
Haim Aflalo <kodkodhaim@gmail.com>
2025-11-11 10:15:14 +0200

    correction on class Library

--- classes/library.py
+++ classes/library.py
@@ -1,46 +1,51 @@
-from classes.book import Book
-from classes.user import User
+from book import Book
+from user import User
 
 
 class Library:
-    def __init__(self, books: list, users: list):
-        self.books = books
-        self.users = users
+    def __init__(self):
+        self.books = {}
+        self.users = {}
 
     def add_book(self, book):
-        self.books.append(book.isbn)
+        self.books[book]= book.__str__()
 
     def add_user(self, user):
-        self.users.append(user.name)
+        self.users[user] = user.__str__()
 
-    @staticmethod
-    def borrow_book(user, book):
-        if book.is_available:
-            user.borrowed_books.append(book.book_isbn)
+    def borrow_book(self,user,book):
+        if user not in self.users:
+            print("the user doesn't exist")
+        if book not in self.books:
+            print("the book doesn't exist")
+        if not book.is_available:
+            print("this book is already borrowed ")
+        else:
+            user.borrowed_books.append(book.isbn)
             book.is_available = False
-        else:
-            print("the book is already borrowed")
 
-    @staticmethod
-    def return_book(user, book):
-        if book.book_isbn in user.borrowed_books:
+    def return_book(self,user, book):
+        if user not in self.users:
+            print("the user doesn't exist")
+        if book not in self.books:
+            print("the book doesn't exist")
+        if book.is_available:
+            print("this book is already returned ")
+        else:
+            user.borrowed_book.remove(book)
             book.is_available = True
-            user.borrowed_books.remove(book.book_isbn)
-        else:
-            print("you didn't have this book")
-
 
     def set_available_books(self):
         available_book = []
-        for i in self.books:
-            if i.is_available:
-                available_book.append(i.isbn)
+        for book in self.books:
+            if book.is_available:
+                available_book.append(book.isbn)
         return available_book
 
     def search_book(self,author):
         book_by_author = []
         for i in self.books:
-            if i == author:
-                book_by_author.append(i)
+            if i.author == author:
+                book_by_author.append(i.__str__())
         return book_by_author
 
--- classes/user.py
+++ classes/user.py
@@ -1,6 +1,11 @@
+from book import Book
 class User:
 
-    def __init__(self, name, id, borrowed_books: list):
+    def __init__(self, name, user_id):
         self.name = name
-        self.id = id
-        self.borrowed_books = borrowed_books
+        self.user_id = user_id
+        self.borrowed_books = []
+
+
+    def __str__(self):
+        return f"user:{self.name}, id:{self.user_id} : {self.borrowed_books}"
